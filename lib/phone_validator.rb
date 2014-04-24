88b80680db10c7712e5318314952999cce2e1e85
James R. Bracy <waratuman@gmail.com>
2014-04-24 11:51:55 -0700

    Fixing validation.

--- lib/phone_validator.rb
+++ lib/phone_validator.rb
@@ -4,37 +4,36 @@ class PhoneValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     return if !value
-    test_value = value
-
-    if options[:extension]
-      test_value = test_value.gsub(/( ?(x\.?|(ext\.?)|extension|ex\.?) ?(\d+))/, '')
-    end
-
-    test_value = "+1#{test_value}" if !Phony.plausible?(test_value)
+    test_value = PhoneValidator.normalize(value, extension: false)
 
     if !Phony.plausible?(test_value)
       record.errors.add(attribute, options[:message] || :invalid)
     end
+
+    # If there is an extension, but shouldn't be
+    if !options[:extension] && test_value != PhoneValidator.normalize(value, extension: true)
+      record.errors.add(attribute, options[:message] || :invalid)
+    end
+
   end
   
   def self.normalize(number, options={})
     return if !number || number.empty?
 
     extension = nil
-    if options[:extension]
-      number =~ /( ?(x\.?|(ext\.?)|extension|ex\.?) ?(\d+))/
-      extension = $4
-      number = number.gsub(/( ?(x\.?|(ext\.?)|extension|ex\.?) ?(\d+))/, '')
-    end
 
-    number = "+1" + number if !Phony.plausible?(number)
+    number =~ /( ?(x\.?|(ext\.?)|extension|ex\.?) ?(\d+))/
+    extension = $4
+    number = number.gsub(/( ?(x\.?|(ext\.?)|extension|ex\.?) ?(\d+))/, '').gsub(/[^\d\+]/, '')
+
+    number = "+1" + number if number[0] != '+' && number.length == 10
 
     return if !Phony.plausible?(number)
 
-    if options[:extension] && extension
-      Phony.normalize(number) + 'x' + extension
+    if options[:extension] != false && extension
+      '+' + Phony.normalize(number) + 'x' + extension
     else
-      Phony.normalize(number)
+      '+' + Phony.normalize(number)
     end
   end
 
